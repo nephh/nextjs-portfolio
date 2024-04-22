3ef8851ec6d0bd0a7d72b7e6694fafd8a068014f
Owen McCormick <owenmccr@gmail.com>
2024-04-22 13:38:55 -0400

    add about section

---
+++ src/pages/about.tsx
@@ -0,0 +1,26 @@
+export default function About() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center text-left">
+      <h2 className="flex flex-nowrap text-lg">About Me</h2>
+      <hr className="my-6 w-4/12 border opacity-25" />
+      <p className="about w-8/12 text-2xl leading-8">
+        Hello! My name is Owen McCormick, I am a junior developer, and I love to
+        create. I am motivated by my love for computers and a desire to leverage
+        technology to create a vast amount of interesting ideas. <br />
+        <br />I have always been partial to computers, and in 2022 I decided I
+        wanted to pursue becoming a programmer as a full-time job. Prior to
+        this, programming had been a small hobby of mine. I begin in C++,
+        learning to mod and manipulate video games, and then moved towards
+        Python in order to create a small Twitter Bot. <br />
+        <br />
+        Since then, I decided to take a bootcamp on web development and really
+        push towards making this a career. My biggest strength is my desire to
+        learn. I have no problem picking up a brand new language, framework, or
+        software and learning the ins and outs of it in order to accomplish my
+        goals. <br />
+        <br />I am incredibly excited to build interesting projects, and
+        continue to learn the ins and outs of being a developer.
+      </p>
+    </div>
+  );
+}
