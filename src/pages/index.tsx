e8709d140d7a7350d0d342354d3e4dfd4641ea8b
Owen McCormick <owenmccr@gmail.com>
2024-04-22 16:31:55 -0400

    finish navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import Head from "next/head";
-import { Link, Element } from "react-scroll";
-import Project from "../components/Project";
+import { Link, Element, scroller, scrollSpy } from "react-scroll";
+import Project from "../components/project";
 import projects from "../data/projectData";
+import { useEffect } from "react";
+import { FloatingNav } from "@/components/ui/navbar";
 
 function ProjectSection() {
   return (
@@ -25,6 +27,33 @@ function ProjectSection() {
   );
 }
 
+function About() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center text-left">
+      <h2 className="flex flex-nowrap text-lg">About Me</h2>
+      <hr className="my-6 w-4/12 border opacity-25" />
+      <p className="about w-8/12 text-2xl leading-8">
+        Hello! My name is Owen McCormick, I am a junior developer, and I love to
+        create. I am motivated by my love for computers and a desire to leverage
+        technology to create a vast amount of interesting ideas. <br />
+        <br />I have always been partial to computers, and in 2022 I decided I
+        wanted to pursue becoming a programmer as a full-time job. Prior to
+        this, programming had been a small hobby of mine. I begin in C++,
+        learning to mod and manipulate video games, and then moved towards
+        Python in order to create a small Twitter Bot. <br />
+        <br />
+        Since then, I decided to take a bootcamp on web development and really
+        push towards making this a career. My biggest strength is my desire to
+        learn. I have no problem picking up a brand new language, framework, or
+        software and learning the ins and outs of it in order to accomplish my
+        goals. <br />
+        <br />I am incredibly excited to build interesting projects, and
+        continue to learn the ins and outs of being a developer.
+      </p>
+    </div>
+  );
+}
+
 function Landing() {
   return (
     <div className="landing flex h-screen flex-col items-center justify-center">
@@ -38,6 +67,9 @@ function Landing() {
           smooth={true}
           duration={1000}
           className="hover:cursor-pointer"
+          href="/#projects"
+          spy={true}
+          hashSpy={true}
         >
           Projects{" "}
         </Link>
@@ -47,6 +79,9 @@ function Landing() {
           smooth={true}
           duration={1000}
           className="hover:cursor-pointer"
+          href="/#about"
+          spy={true}
+          hashSpy={true}
         >
           About
         </Link>
@@ -56,6 +91,33 @@ function Landing() {
 }
 
 export default function Home() {
+  useEffect(() => {
+    const hash = window.location.hash.substring(1);
+    if (hash) {
+      scroller.scrollTo(hash, {
+        duration: 800,
+        delay: 0,
+        smooth: "easeInOutQuart",
+      });
+    }
+    scrollSpy.update();
+  }, []);
+
+  const navItems = [
+    {
+      name: "Home",
+      link: "/",
+    },
+    {
+      name: "About",
+      link: "/about",
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+    },
+  ];
+
   return (
     <>
       <Head>
@@ -64,12 +126,15 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="text-md flex flex-col items-center justify-center bg-slate-900 text-center text-slate-200">
+        <FloatingNav navItems={navItems} />
         <div className="w-full">
           <Landing />
           <Element name="projects">
             <ProjectSection />
           </Element>
-          <Element name="about">{/* <About /> */}</Element>
+          <Element name="about">
+            <About />
+          </Element>
         </div>
       </main>
     </>
